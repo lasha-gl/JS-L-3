bdd959cb367a29eab255624343edd451320d0fda
lasha-gl <gegechkori19@gmail.com>
2021-11-12 14:08:32 +0400

    add1

---
+++ script.js
@@ -0,0 +1,68 @@
+// #1
+
+
+// let array = [5, 25, 89, 120, 36];
+
+// array.push ('jvascript', 'python')
+// array.unshift ('html', 'css')
+// console.log(array.length);
+// array.shift();
+// array.pop();
+// console.log(array);
+
+// #2
+
+// let array = ['ფორთოხალი', 'ბანანი', 'მსხალი'];
+// console.log(array.length);
+// array.push ('ვაშლი', 'ანანასი');
+// array.unshift ('საზამთრო');
+// array.splice (2,0, 'მანგო');
+// array.shift();
+// array.pop();
+// console.log(array);
+
+
+// #3
+// let array = [12, 25, 3, 6, 8, 14, 7, 23];
+//  let newarray = array.map (item => item / 3);
+//  console.log(newarray);
+
+
+// #4 
+// let array = ["hello", 125, "javascript", "html", 43, "css", "scss", "bootstrap", 88, 59, "python"];
+//  let newarray = array.filter(function (item) {
+//     if (typeof item === "number") {
+//         console.log(item);
+//     }
+//   });
+
+
+// #5
+// let languages = ['html', 'css', 'javascript', 'python', 'php'];
+// let newlanguages = languages.filter(items => items.length > 3);
+// console.log(newlanguages);
+
+
+// #6
+// let array= ['academy', 'of', 'digital', 'industries'].reduce ((a,c) => a + ' ' + c, ' ');
+// console.log(array);
+
+// #7
+// let item = [12, 'google', 32, null, 'yahoo', 25];
+// let newitems = item.map(item =>{
+//     if (typeof item == 'number') {
+//         return item * item;
+//     } else if  (typeof item == 'string'){
+//         return item.toUpperCase();
+//     }
+// else {
+//         return item;
+// }})
+// console.log(newitems);
+
+
+// #8
+// let words = ['Madrid', 'Rome', 'Milan', 'Berlin'];
+// let newwords = words.filter(i => i.includes('m') || i.includes('M') )
+
+// console.log(newwords);
